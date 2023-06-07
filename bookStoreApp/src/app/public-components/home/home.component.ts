4b6cdb26b62e828ac39f24d871e8f8f5f8727267
rverdera <rverdera@vertere-gs.com>
2023-06-07 14:45:29 +0800

    @Output and EventEmitter

--- bookStoreApp/src/app/public-components/home/home.component.ts
+++ bookStoreApp/src/app/public-components/home/home.component.ts
@@ -1,47 +1,21 @@
-import { Component, Inject, OnInit, ViewChild, AfterViewInit, ElementRef} from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { AuthorsComponent } from 'src/app/shared/components/authors/authors.component';
-import { CounterService } from 'src/app/shared/services/counter.service';
+import { Component, Inject, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
 
-  @ViewChild('authors') private childAuthor: AuthorsComponent;
-  @ViewChild('decButton') private decButton: MatButton;
-  @ViewChild('title') private title: ElementRef;
 
-  constructor(public _counterService: CounterService,
-    @Inject('appTitle') public config: any
-    ) { }
+
+  constructor() { }
 
   ngOnInit(): void {
 
   }
 
-  ngAfterViewInit(): void {
-
-   setTimeout(() => {
-
-    this.childAuthor.setData(10);
-    this.decButton.color = 'primary';
-    this.decButton.disabled = true;
-    this.title.nativeElement.innerHTML = 'This is updated title';
-    console.log(this.title);
-    
-   }, 0);
-
-  }
-
-  public increase(): void {
-    this._counterService.incCounter();
-  }
-
-  public decrease(): void {
-    this._counterService.decCounter();
-  }
-
+  public childData($event: any): void {
+    console.log($event);    
+  }  
 }
--- bookStoreApp/src/app/shared/components/authors/authors.component.ts
+++ bookStoreApp/src/app/shared/components/authors/authors.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-authors',
@@ -6,6 +6,8 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./authors.component.scss']
 })
 export class AuthorsComponent implements OnInit {
+
+  @Output() myData = new EventEmitter<string>();
 
   public data: number;
 
@@ -16,6 +18,10 @@ export class AuthorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  btnClick(): void {
+    this.myData.emit('This text is from child component');
   }
 
 }
