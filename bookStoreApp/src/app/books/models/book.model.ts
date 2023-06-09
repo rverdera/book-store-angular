bcde84fe39bf99f7a853572d9cd8d64a3cc2f05f
rverdera <rverdera@vertere-gs.com>
2023-06-09 16:39:27 +0800

    Angular two way data binding using ngModel Explained with example

--- bookStoreApp/src/app/books/models/book.model.ts
+++ bookStoreApp/src/app/books/models/book.model.ts
@@ -4,18 +4,9 @@ export class BookModel {
     public id: number;
     public title: string;
     public totalPages: number;
-    private _author: string;
+    public author: string;
     public price: PriceModel;
     public isPublished: boolean;
     public publishedOn: Date;
-
-    public set author(value: string) {
-        this._author = value;
-    }
-
-    public get author() {
-        return this._author;
-    }
-
 
 }
