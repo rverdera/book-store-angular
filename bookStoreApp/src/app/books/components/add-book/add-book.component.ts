04b04f7c90e47f3df0c6efa4168eda34299fd387
rverdera <rverdera@vertere-gs.com>
2023-06-09 15:08:32 +0800

    Template driven form submit in angular | Angular Forms

--- bookStoreApp/src/app/books/components/add-book/add-book.component.ts
+++ bookStoreApp/src/app/books/components/add-book/add-book.component.ts
@@ -1,22 +1,31 @@
-import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {Component, ElementRef, OnInit } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { BookModel } from '../../models/book.model';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
   styleUrls: ['./add-book.component.scss']
 })
-export class AddBookComponent implements OnInit, AfterViewChecked {
+export class AddBookComponent implements OnInit {
 
-  @ViewChild('myForms') myForms: ElementRef;
 
-  constructor() { }
 
-  ngAfterViewChecked(): void {
-    console.log(this.myForms);
-    
-  }
+  constructor(private _bookService: BookService) { }
+
 
   ngOnInit(): void {
+  }
+
+  saveBook(value: any) : void {
+    console.log(value);
+    const book: BookModel = new BookModel();
+    book.title = value.title;
+    book.author = value.author;
+    book.totalPages = value.pages;
+    book.price = value.price;
+
+    this._bookService.addBook(book);
   }
 
 }
--- bookStoreApp/src/app/books/services/book.service.ts
+++ bookStoreApp/src/app/books/services/book.service.ts
@@ -6,81 +6,85 @@ import { BookModel } from '../models/book.model';
 })
 export class BookService {
 
+
+  private allBooks = [
+    {
+      "id": 1,
+      "title": "Angular Fundamentals",
+      "totalPages": 453,
+      "author": "Ram Singh",
+      "price": {
+        "currency": "INR",
+        "value": 199
+      }
+    },
+    {
+      "id": 2,
+      "title": "Java Fundamentals",
+      "totalPages": 987,
+      "author": "Kishan Pal",
+      "price": {
+        "currency": "USD",
+        "value": 19
+      }    
+    },
+    {
+      "id": 3,
+      "title": "The ups and downs of life",
+      "totalPages": 456,
+      "author": "Jay Malhotre",
+      "price": {
+        "currency": "INR",
+        "value": 299
+      }    
+    },
+    {
+      "id": 4,
+      "title": "Azure Walkthrough",
+      "totalPages": 879,
+      "author": "Pintu Verma",
+      "price": {
+        "currency": "INR",
+        "value": 399
+      }    
+    },
+    {
+      "id": 5,
+      "title": "Asp.Net Core Web API",
+      "totalPages": 546,
+      "author": "Nagesh Khatri",
+      "price": {
+        "currency": "USD",
+        "value": 99
+      }    
+    },
+    {
+      "id": 6,
+      "title": "Azure Walkthrough",
+      "totalPages": 879,
+      "author": "Pintu Verma",
+      "price": {
+        "currency": "INR",
+        "value": 399
+      }    
+    },
+    {
+      "id": 7,
+      "title": "Asp.Net Core Web API",
+      "totalPages": 546,
+      "author": "Nagesh Khatri",
+      "price": {
+        "currency": "USD",
+        "value": 99
+      }    
+    }
+  ];
+
   constructor() { }
 
+
   public getBooks(): any[] {
-    return [
-      {
-        "id": 1,
-        "title": "Angular Fundamentals",
-        "totalPages": 453,
-        "author": "Ram Singh",
-        "price": {
-          "currency": "INR",
-          "value": 199
-        }
-      },
-      {
-        "id": 2,
-        "title": "Java Fundamentals",
-        "totalPages": 987,
-        "author": "Kishan Pal",
-        "price": {
-          "currency": "USD",
-          "value": 19
-        }    
-      },
-      {
-        "id": 3,
-        "title": "The ups and downs of life",
-        "totalPages": 456,
-        "author": "Jay Malhotre",
-        "price": {
-          "currency": "INR",
-          "value": 299
-        }    
-      },
-      {
-        "id": 4,
-        "title": "Azure Walkthrough",
-        "totalPages": 879,
-        "author": "Pintu Verma",
-        "price": {
-          "currency": "INR",
-          "value": 399
-        }    
-      },
-      {
-        "id": 5,
-        "title": "Asp.Net Core Web API",
-        "totalPages": 546,
-        "author": "Nagesh Khatri",
-        "price": {
-          "currency": "USD",
-          "value": 99
-        }    
-      },
-      {
-        "id": 4,
-        "title": "Azure Walkthrough",
-        "totalPages": 879,
-        "author": "Pintu Verma",
-        "price": {
-          "currency": "INR",
-          "value": 399
-        }    
-      },
-      {
-        "id": 5,
-        "title": "Asp.Net Core Web API",
-        "totalPages": 546,
-        "author": "Nagesh Khatri",
-        "price": {
-          "currency": "USD",
-          "value": 99
-        }    
-      }
-    ]
+    return this.allBooks;
   }
 
   public recentBooks(): any[] {
@@ -106,6 +110,10 @@ export class BookService {
         }    
       }
     ]
+  }
+
+  public addBook(bookModel : BookModel): void {
+    this.allBooks.push(bookModel);
   }
 
 }
